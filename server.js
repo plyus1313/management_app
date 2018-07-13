c105983037995041202a6ba1a769fa8f1349de25
Viktoriya Savaryn <vsviktoriya13@gmail.com>
2018-07-13 16:10:06 +0300

    commit back-end

---
+++ server.js
@@ -0,0 +1,150 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const knex = require("knex");
+const token = "6f73b60749e250e3963341bfb7d84204";
+
+const db = knex({
+  client: "pg",
+  connection: {
+    host: "127.0.0.1",
+    user: "postgres",
+    password: "pass123",
+    database: "management",
+    timezone: "UTC"
+  }
+});
+
+//Helpers
+function totalEUR() {
+  return new Promise((resolve, reject) => {
+    return db("person")
+      .select(knex.raw("SUM(money) as total"))
+      .first()
+      .then(data => {
+        console.log(data);
+        resolve(data["total"]);
+      })
+      .catch(err => {
+        reject(err);
+      });
+  });
+}
+
+function convert(sum, to, reverse) {
+  to = to.toUpperCase();
+  return new Promise((resolve, reject) => {
+    const request = require("request");
+
+    request(
+      "http://data.fixer.io/api/latest?access_key=" + token + "&symbols=" + to,
+      function(err, res, body) {
+        if (err) {
+          console.log(to);
+          return reject(err);
+        }
+        data = JSON.parse(body);
+        console.log(data);
+
+        rate = data["rates"][to];
+        console.log(sum / rate);
+        if (reverse) return resolve(sum / rate);
+        return resolve(sum * rate);
+      }
+    );
+  });
+}
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(cors());
+
+//Routes
+app.get("/", (req, res) => {
+  db("person")
+    .returning("*")
+    .orderBy("expense", "desc")
+    .then(data => {
+      data = data.map(item => {
+        item["expense"] = new Date(item["expense"]);
+        item["expense"].setTime(
+          item["expense"].getTime() -
+            item["expense"].getTimezoneOffset() * 60 * 1000
+        );
+        item["expense"] =
+          item["expense"].getFullYear() +
+          "-" +
+          (item["expense"].getMonth() + 1 < 10
+            ? "0" + (item["expense"].getMonth() + 1)
+            : item["expense"].getMonth() + 1) +
+          "-" +
+          (item["expense"].getDate() < 10
+            ? "0" + item["expense"].getDate()
+            : item["expense"].getDate());
+
+        return item;
+      });
+      res.json(data);
+    });
+});
+
+app.get("/total", (req, res) => {
+  totalEUR()
+    .then(total => {
+      convert(total, req.query.currency || "PLN").then(result => {
+        console.log("RES: " + result);
+        res.json({ total: result });
+      });
+    })
+    .catch(err => {
+      console.log(err);
+    });
+});
+
+app.put("/add", (req, res) => {
+  let { name, money, currency, expense } = req.body;
+  let date = new Date(expense);
+  convert(money, currency || "PLN", true)
+    .then(money_converted => {
+      console.log("RES: " + money_converted);
+      db("person")
+        .returning("*")
+        .insert({
+          name: name,
+          money: money_converted,
+          currency: "EUR",
+          expense: date
+        })
+        .then(data => {
+          res.json(data);
+        })
+        .catch(err => res.status(400).json(err));
+    })
+    .catch(err => res.status(400).json(err));
+});
+
+app.delete("/clear", (req, res) => {
+  let expense = req.query.expense;
+  let date = new Date(expense);
+  date.setHours(0);
+  let maxDate = new Date(expense);
+  maxDate.setHours(23);
+  maxDate.setMinutes(59);
+  maxDate.setSeconds(59);
+
+  db("person")
+    .delete()
+    .whereBetween("expense", [date, maxDate])
+    .then(response => {
+      res.json(response);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+      console.log(err);
+    });
+});
+
+app.listen(3000, () => {
+  console.log("app is running on port 3000");
+});
---
+++ tests/tsuite.js
@@ -0,0 +1,64 @@
+const chai = require("chai");
+var chaiHttp = require('chai-http');
+var should = chai.should();
+var assert = require('chai').assert;
+var expect = require('chai').expect;
+
+const url = "http://localhost:3000";
+
+chai.use(chaiHttp);
+
+describe('Blobs', function() {
+  it('should list all expenses / GET');
+  it('should successfully add expense / PUT');
+  it('should successfully delete expense /clear DELETE');
+})
+
+it('should list all expenses / GET',function(done){
+  chai.request(url)
+  .get("/")
+  .end(function(err, res){
+    res.should.have.status("200");
+    res.body.should.be.a("array");
+    assert.isAtLeast(res.body.length,1,"Length is " + res.body.length);
+    done();
+  });
+})
+
+it('should successfully add expense /add PUT',function (done) {
+    chai.request(url)
+    .put("/add")
+    .send({"name":"test_name","money":15,"currency":"EUR","expense":"2099-07-25"})
+    .end(function (err, res) {
+      res.should.have.status("200");
+      chai.request(url)
+      .get("/")
+      .end(function(err, res){
+        console.log(res.body);
+        res.should.have.status("200");
+        res.body.should.be.a("array");
+        expect(res.body).to.deep.include.members([{"name":"test_name","money":15,"currency":"EUR","expense":"2099-07-25"}]);
+        done();
+      });
+    })
+});
+
+it('should successfully delete expense /clear DELETE',function (done) {
+    chai.request(url)
+    .delete("/clear?expense=2099-07-25")
+    .send({"expense":"2099-07-25"})
+    .end(function (err, res) {
+      console.log(res.body);
+      res.should.have.status("200");
+      chai.request(url)
+      .get("/")
+      .end(function(err, res){
+        console.log(res.body);
+        res.should.have.status("200");
+        res.body.should.be.a("array");
+        expect(res.body).to.not.deep.include.members([{"expense":"2099-07-25"}]);
+        console.log(res.body);
+        done();
+      });
+    })
+});
